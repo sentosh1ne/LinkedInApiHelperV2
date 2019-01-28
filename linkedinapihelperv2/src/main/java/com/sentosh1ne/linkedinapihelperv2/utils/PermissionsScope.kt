6f593d420b48147b03117c56abe0ade19ae2ecbe
Stanislav Vylegzhanin <stanislav.vylegzhanin@airlab.ai>
2019-01-28 13:04:49 +0200

    Initial setup Permissions scope added

---
+++ linkedinapihelperv2/src/main/java/com/sentosh1ne/linkedinapihelperv2/utils/PermissionsScope.kt
@@ -0,0 +1,59 @@
+package com.sentosh1ne.linkedinapihelperv2.utils
+
+import java.util.*
+
+class PermissionsScope(vararg permissions: String) {
+
+    private val permissions = HashSet<String>()
+
+    init {
+        this.permissions.addAll(permissions)
+    }
+
+    override fun toString(): String {
+        val scope = StringBuilder()
+        permissions.forEachIndexed { index, item ->
+            if (index == 0) {
+                scope.append(item)
+            } else {
+                scope.append("+$item")
+            }
+        }
+
+        return scope.toString()
+    }
+}
+
+object Scopes {
+    /**
+     * Your Profile Overview 	Name, photo, headline, and current positions
+     */
+    val R_BASICPROFILE = "r_basicprofile"
+
+    /**
+     * Your Full Profile 	Full profile including experience, education, skills, and recommendations
+     */
+    val R_FULLPROFILE = "r_fullprofile"
+
+
+    val R_LITEPROFILE = "r_liteprofile"
+
+    /**
+     * Your Email Address 	The primary email address you use for your LinkedIn account
+     */
+    val R_EMAILADDRESS = "r_emailaddress"
+
+    /**
+     * Your Contact Info 	Address, phone number, and bound accounts
+     */
+    val R_CONTACTINFO = "r_contactinfo"
+    /**
+     * Company Page & Analytics 	Edit company pages for which I am an Admin and post status updates on behalf of those companies
+     */
+    val RW_COMPANY_ADMIN = "rw_company_admin"
+
+    /**
+     * Share, comment & like    Post updates, make comments and like posts
+     */
+    val W_SHARE = "w_share"
+}
